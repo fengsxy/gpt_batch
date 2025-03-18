f85b2fde38f50fd16ddf1c8846a80a0f85cd7d42
fengxyqs <4797136@qq.com>
2025-03-17 21:27:35 -0700

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gpt_batch',
-    version='0.1.7',
+    version='0.1.8',
     packages=find_packages(),
     install_requires=[
         'openai', 'tqdm','anthropic'
